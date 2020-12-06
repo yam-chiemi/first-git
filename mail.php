dcbb6e9673525f40b85540621cd121dd5dc43c6c
yam-chiemi <yam-chiemi@gmail.com>
2020-12-06 14:02:51 +0900

    favicon

--- mail.php
+++ mail.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+   <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <?php
+
+mb_language("ja");
+mb_internal_encoding("UTF-8");
 
 $request_param = $_POST;
 
@@ -17,7 +26,7 @@ $content .= "=================================\r\n";
 $content .= "お名前 " . htmlspecialchars($request_param['name'])."\r\n";
 $content .= "会社名 " . htmlspecialchars($request_param['company'])."\r\n";
 $content .= "メールアドレス " . htmlspecialchars($request_param['email'])."\r\n";
-$content .= "お問い合わせ内容 " . htmlspecialchars($request_param['message'])."\r\n";
+$content .= "お問い合わせ内容 " . htmlspecialchars($request_param['content'])."\r\n";
 $content .= "お問い合わせ日時 " . $request_datetime."\r\n";
 $content .= "=================================\r\n";
 
@@ -29,27 +38,25 @@ $content2 .= "=================================\r\n";
 $content2 .= "お名前 " . htmlspecialchars($request_param['name'])."\r\n";
 $content2 .= "会社名 " . htmlspecialchars($request_param['company'])."\r\n";
 $content2 .= "メールアドレス " . htmlspecialchars($request_param['email'])."\r\n";
-$content2 .= "お問い合わせ内容 " . htmlspecialchars($request_param['message'])."\r\n";
+$content2 .= "お問い合わせ内容 " . htmlspecialchars($request_param['content'])."\r\n";
 $content2 .= "お問い合わせ日時   " . $request_datetime."\r\n";
 $content2 .= "================================="."\r\n";
 
-mb_language("ja");
-mb_internal_encoding("UTF-8");
+
 //mail 送信
-if($request_param['token'] === '1234567'){
-if(mb_send_mail($to, $subject2, $content2, $mailfrom)){
-   mb_send_mail($mailto,$subject,$content,$mailfrom);
+
+if(mb_send_mail($to, $subject2,header('Content-Type: text/html; charset=UTF-8'),$content2)){
+   mb_send_mail($mailto,$subject,header('Content-Type: text/html; charset=UTF-8'),$content);
    ?>
    <script>
-       window.location = "https://d2132e2338594f789a213b0b3cc73f72.vfs.cloud9.us-east-1.amazonaws.com/_static/original-site/index.html?name=%E5%B1%B1%E7%94%B0%E5%8D%83%E7%B5%B5%E7%BE%8E&company=&email=white-sesame%40ac.auone-net.jp&comment=#contact";
+       window.location = "http://www.yama-chi.sakura.ne.jp/wd/thanks";
    </script>
    <?php
 } else {
    header('Content-Type: text/html; charset=UTF-8');
- echo "メールの送信に失敗しました";
+   echo "メールの送信に失敗しました";
 };
-} else {
-echo "メールの送信に失敗しました（トークンエラー）";
-}
 
 ?>
+</body>
+</html>
